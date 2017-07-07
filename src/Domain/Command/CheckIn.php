08c80605afa7954622d247345cb6d2e6f0b7c0c1
Fabian Picone <fabian.picone@easylearn.ch>
2017-07-07 15:51:11 +0200

    Check in with username

---
+++ src/Domain/Command/CheckIn.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types=1);
+
+namespace Building\Domain\Command;
+
+use Prooph\Common\Messaging\Command;
+use Rhumsaa\Uuid\Uuid;
+
+final class CheckIn extends Command
+{
+    private $username;
+    private $buildingId;
+
+    private function __construct(string $username, Uuid $buildingId)
+    {
+        $this->init();
+
+        $this->username = $username;
+        $this->buildingId = $buildingId;
+    }
+
+    public static function fromUserName(string $username, Uuid $buildingId): self
+    {
+        return new self($username, $buildingId);
+    }
+
+    public function username(): string
+    {
+        return $this->username;
+    }
+
+    public function buildingId(): Uuid
+    {
+        return $this->buildingId;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function payload(): array
+    {
+        return [
+            'username' => $this->username,
+            'buildingId' => $this->buildingId
+        ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setPayload(array $payload)
+    {
+        $this->username = $payload['username'];
+        $this->buildingId = $payload['buildingId'];
+    }
+}
