c1a69eda652335615c8b18ac8562063146e1ca7e
Fabian Picone <fabian.picone@gmail.com>
2017-07-12 08:43:19 +0200

    Get checked-in users for dumping them (projection)

---
+++ src/Infrastructure/Projector/CheckedInUserProjectionWriter.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace Building\Infrastructure\Projector;
+
+use Rhumsaa\Uuid\Uuid;
+
+final class CheckedInUserProjectionWriter
+{
+    public function write(Uuid $aggregateId, array $usernames): void
+    {
+        $folder = __DIR__ . '/../../public/building/dump/';
+        mkdir($folder, 0775, true);
+        file_put_contents(
+            $folder . 'building-dump-' . $aggregateId->toString() . '.json',
+            json_encode($usernames)
+        );
+    }
+}
---
+++ src/Infrastructure/Projector/CheckedInUserProjector.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+namespace Building\Infrastructure;
+
+use Building\Domain\DomainEvent\UserWasCheckedIn;
+use Building\Domain\DomainEvent\UserWasCheckedOut;
+use Building\Infrastructure\Projector\CheckedInUserProjectionWriter;
+use Prooph\EventSourcing\AggregateChanged;
+use Prooph\EventStore\EventStore;
+use Prooph\EventStore\Stream\StreamName;
+use Rhumsaa\Uuid\Uuid;
+
+final class CheckedInUserProjector
+{
+    private $eventStore;
+    private $writer;
+
+    public function __construct(EventStore $eventStore, CheckedInUserProjectionWriter $writer)
+    {
+        $this->eventStore = $eventStore;
+        $this->writer = $writer;
+    }
+
+    public function project(AggregateChanged $event): void
+    {
+        $aggregateId = Uuid::fromString($event->aggregateId());
+        $recordedEvents = $this->eventStore->loadEventsByMetadataFrom(
+            new StreamName('event_stream'),
+            [
+                'aggregate_id' => $aggregateId,
+            ]
+        );
+
+        $this->writer->write($aggregateId, $this->getCheckedInUsernames($recordedEvents));
+    }
+
+    private function getCheckedInUsernames(\Iterator $recordedEvents): array
+    {
+        $usernames = [];
+        foreach ($recordedEvents as $recordedEvent) {
+            if ($recordedEvent instanceof UserWasCheckedIn) {
+                $usernames[$recordedEvent->username()] = null;
+            }
+            if ($recordedEvent instanceof UserWasCheckedOut) {
+                unset($usernames[$recordedEvent->username()]);
+            }
+        }
+        return array_keys($usernames);
+    }
+}
